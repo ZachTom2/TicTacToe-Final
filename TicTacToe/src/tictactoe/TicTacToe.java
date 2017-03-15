df7afd2a4af1482d75e111a6a5e8fab51a2b9a68
Robert Plafker <robertplafker@icloud.com>
2017-03-15 12:04:57 -0700

    Added Robbie

--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -17,6 +17,9 @@ public class TicTacToe {
     public static void main(String[] args) {
         // TODO code application logic here
         //sasha is here
+<<<<<<< HEAD
+        //robbie is the best student ever
+=======
     }
     
 }
