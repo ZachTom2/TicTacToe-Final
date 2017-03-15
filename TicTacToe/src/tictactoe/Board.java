d6c1e345f880852b57ad5f5ad365d63e6c238147
Robert Plafker <robertplafker@icloud.com>
2017-03-15 12:46:05 -0700

    Finished the board abstract class
    Probably still needs work to get going though

--- TicTacToe/src/tictactoe/Board.java
+++ TicTacToe/src/tictactoe/Board.java
@@ -10,5 +10,18 @@ package tictactoe;
  * @author Robbie
  */
 public abstract class Board {
-    
+    String[][] board=new String[3][3];
+    public void printBoard(){
+        for (int i=0;i<board.length;i++){
+            for (int j=0;j<board[i].length;j++){
+                System.out.print(board[i][j]+"|");
+            }
+            System.out.println();
+            System.out.println("__  __  __");
+        }
+        
+    }
+    abstract void placex();
+    abstract void placeo();
+    abstract String checkWin();
 }
