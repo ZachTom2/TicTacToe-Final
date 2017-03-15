7baee76bb94031a6ce78fde40ccce843a1f39a80
Robert Plafker <robertplafker@icloud.com>
2017-03-15 12:09:15 -0700

    Deleted Sasha

--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -16,10 +16,7 @@ public class TicTacToe {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //sasha is here
-<<<<<<< HEAD
         //robbie is the best student ever
-=======
     }
     
 }
